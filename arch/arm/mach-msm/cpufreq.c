92c3fbaf23571a3f868ada1c2a866a0794675ea9
Praveen Chidambaram <pchidamb@codeaurora.org>
2012-05-25 17:29:11 -0600

    msm: cpufreq: Add API to allow limiting of min and max cpu frequencies
    CPUFreq provides scaling sysfs nodes to set min and max frequencies, but
    there are times when the operating range has to be controlled
    irrespective of the scaling limits, to ensure the thermal stability of
    the device.
    
    The API allows kernel thermal safeguards to pitch in and limit the
    frequencies.
    
    Change-Id: I3dc7496747792e6223695131f1644dd87054b025
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -36,8 +36,9 @@
 
 #ifdef CONFIG_HTC_DEBUG_FOOTPRINT
 #include <mach/htc_footprint.h>
-#endif
 #include <mach/clk-provider.h>
+#endif
+#include <mach/cpufreq.h>
 
 #include "acpuclock.h"
 
@@ -54,15 +55,6 @@ static struct clk *l2_clk;
 static unsigned int freq_index[NR_CPUS];
 static unsigned int max_freq_index;
 static struct cpufreq_frequency_table *freq_table;
-#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
-static struct cpufreq_frequency_table *krait_freq_table;
-#endif
-
-#ifdef CONFIG_EDP_LIMIT
-static unsigned int max_freq_table_index; 
-static unsigned int freq_req_cnt;
-#endif
-
 static unsigned int *l2_khz;
 static bool is_clk;
 static bool is_sync;
@@ -132,37 +124,41 @@ out:
 #ifdef CONFIG_ARCH_MSM8974
 static DEFINE_MUTEX(set_cpufreq_lock);
 #endif
+
+struct cpu_freq {
+	uint32_t max;
+	uint32_t min;
+	uint32_t allowed_max;
+	uint32_t allowed_min;
+	uint32_t limits_init;
+};
+
+static DEFINE_PER_CPU(struct cpu_freq, cpu_freq_info);
+
 static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq,
 			unsigned int index)
 {
 	int ret = 0;
-#ifdef CONFIG_EDP_LIMIT
-	int cpu;
-	int cpu_cnt = 0;
-#endif
 	int saved_sched_policy = -EINVAL;
 	int saved_sched_rt_prio = -EINVAL;
 	struct cpufreq_freqs freqs;
 	struct sched_param param = { .sched_priority = MAX_RT_PRIO-1 };
+	struct cpu_freq *limit = &per_cpu(cpu_freq_info, policy->cpu);
+
+	if (limit->limits_init) {
+		if (new_freq > limit->allowed_max) {
+			new_freq = limit->allowed_max;
+			pr_debug("max: limiting freq to %d\n", new_freq);
+		}
+
+		if (new_freq < limit->allowed_min) {
+			new_freq = limit->allowed_min;
+			pr_debug("min: limiting freq to %d\n", new_freq);
+		}
+	}
 
 	if (policy->cpu >= 1 && is_sync)
 		return 0;
-
-#ifdef CONFIG_EDP_LIMIT
-	if (edp_limit) {
-		for_each_online_cpu(cpu)
-			cpu_cnt++;
-		if (cpu_cnt > 1 && index >= max_freq_table_index - 1) {
-			freq_req_cnt++;
-			if (freq_req_cnt > edp_limit) {
-				freq_req_cnt--;
-				index = max_freq_table_index - 1;
-				new_freq = freq_table[index].frequency;
-			}
-		} else if ((cpu_cnt <= 1 || index < 7) && freq_req_cnt > 0)
-			freq_req_cnt--;
-	}
-#endif
 
 #ifdef CONFIG_ARCH_MSM8974
 	mutex_lock(&set_cpufreq_lock);
@@ -296,6 +292,67 @@ static unsigned int msm_cpufreq_get_freq(unsigned int cpu)
 
 	return acpuclk_get_rate(cpu);
 }
+
+static inline int msm_cpufreq_limits_init(void)
+{
+	int cpu = 0;
+	int i = 0;
+	struct cpufreq_frequency_table *table = NULL;
+	uint32_t min = (uint32_t) -1;
+	uint32_t max = 0;
+	struct cpu_freq *limit = NULL;
+
+	for_each_possible_cpu(cpu) {
+		limit = &per_cpu(cpu_freq_info, cpu);
+		table = cpufreq_frequency_get_table(cpu);
+		if (table == NULL) {
+			pr_err("%s: error reading cpufreq table for cpu %d\n",
+					__func__, cpu);
+			continue;
+		}
+		for (i = 0; (table[i].frequency != CPUFREQ_TABLE_END); i++) {
+			if (table[i].frequency > max)
+				max = table[i].frequency;
+			if (table[i].frequency < min)
+				min = table[i].frequency;
+		}
+		limit->allowed_min = min;
+		limit->allowed_max = max;
+		limit->min = min;
+		limit->max = max;
+		limit->limits_init = 1;
+	}
+
+	return 0;
+}
+
+int msm_cpufreq_set_freq_limits(uint32_t cpu, uint32_t min, uint32_t max)
+{
+	struct cpu_freq *limit = &per_cpu(cpu_freq_info, cpu);
+
+	if (!limit->limits_init)
+		msm_cpufreq_limits_init();
+
+	if ((min != MSM_CPUFREQ_NO_LIMIT) &&
+		min >= limit->min && min <= limit->max)
+		limit->allowed_min = min;
+	else
+		limit->allowed_min = limit->min;
+
+
+	if ((max != MSM_CPUFREQ_NO_LIMIT) &&
+		max <= limit->max && max >= limit->min)
+		limit->allowed_max = max;
+	else
+		limit->allowed_max = limit->max;
+
+	pr_debug("%s: Limiting cpu %d min = %d, max = %d\n",
+			__func__, cpu,
+			limit->allowed_min, limit->allowed_max);
+
+	return 0;
+}
+EXPORT_SYMBOL(msm_cpufreq_set_freq_limits);
 
 static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 {
@@ -525,12 +582,6 @@ static int cpufreq_parse_dt(struct device *dev)
 		if (i > 0 && f <= freq_table[i-1].frequency)
 			break;
 
-#ifdef CONFIG_EXT_CMD_LINE
-		if (f > arg_cpu_oc) {
-			nf = i;
-			break;
-		}
-#endif		
 		freq_table[i].index = i;
 		freq_table[i].frequency = f;
 
@@ -551,23 +602,6 @@ static int cpufreq_parse_dt(struct device *dev)
 
 	freq_table[i].index = i;
 	freq_table[i].frequency = CPUFREQ_TABLE_END;
-#ifdef CONFIG_EXT_CMD_LINE
-	max_freq_table_index = i - 1;	
-#endif
-	
-#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
-	/* Create frequence table with unrounded values */
-	krait_freq_table = devm_kzalloc(dev, (nf + 1) * sizeof(*krait_freq_table),
-					GFP_KERNEL);
-	if (!krait_freq_table)
-		return -ENOMEM;
-
-	*krait_freq_table = *freq_table;
-
-	for (i = 0, j = 0; i < nf; i++, j += 3)
-		krait_freq_table[i].frequency = data[j];
-	krait_freq_table[i].frequency = CPUFREQ_TABLE_END;
-#endif
 
 	devm_kfree(dev, data);
 
@@ -608,26 +642,6 @@ const struct file_operations msm_cpufreq_fops = {
 	.llseek		= seq_lseek,
 	.release	= seq_release,
 };
-#endif
-
-#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
-int use_for_scaling(unsigned int freq)
-{
-	unsigned int i, cpu_freq;
-
-	if (!krait_freq_table)
-		return -EINVAL;
-
-	for (i = 0; krait_freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
-		cpu_freq = krait_freq_table[i].frequency;
-		if (cpu_freq == CPUFREQ_ENTRY_INVALID)
-			continue;
-		if (freq == cpu_freq)
-			return freq;
-	}
-
-	return -EINVAL;
-}
 #endif
 
 static int __init msm_cpufreq_probe(struct platform_device *pdev)
